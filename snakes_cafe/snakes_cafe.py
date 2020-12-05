dfab32358c39cfa7d99ebffb03171a1faecb8ad1
Sian <sianculligan@gmail.com>
2020-12-05 13:37:17 -0800

    scaffolding project

---
+++ snakes_cafe/snakes_cafe.py
@@ -0,0 +1,26 @@
+# TODO When run, the program should print an intro message and the menu for the restaurant
+# TODO The restaurant’s menu should include appetizers, entrees, desserts, and beverages.
+# TODO The program should prompt the user for an order
+# TODO When a user enters an item, the program should print an acknowledgment of their input
+# TODO The program should tell the user how to exit
+# TODO The program’s content should match included sample exactly
+# TODO Actually, there’s one tiny spot that should be different - see if you can spot it.
+# TODO The > character represents user input line and should be printed out with a trailing space.
+
+
+print("**************************************")
+print("**    Welcome to the Snakes Cafe!   **")
+print("**    Please see our menu below.    **")
+print("** ")
+print('** To quit at any time, type \"quit\" **')
+print("**************************************")
+
+print("Appetizers")
+print("----------")
+print("Wings")
+print("Cookies")
+print("Spring Rolls")
+print(" this is a line")
+print()
+print()
+
